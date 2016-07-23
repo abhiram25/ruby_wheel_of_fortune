5ba92f33ea282eecee980b641fb1c7c5eb80ecef
Abhi Reddy <abhi.reddy1@gmail.com>
2016-07-23 00:46:38 -0400

    Added player

---
+++ pseudo.md
@@ -0,0 +1,61 @@
+1. Game is displayed
+2. Hint is displayed
+3. Player and computer start at $0
+4. Player spins
+   a. If player spins and the result is bankrupt, his money goes to 0
+   	  then computer's turn
+   b. If player spins and the result is Lose a turn, then it becomes
+      the computer's turn.
+   c. If the player spins and gets a money value, it should be set to a
+      variable that stores it as pending money.
+5. Ask if player wants pick a letter or guess the phrase
+   a. Player picks a letter
+      1. Player cannot pick a vowel if he has less than $250
+      2. If letter is guessed correctly, then pending money is added to
+         player's money
+   	      i. Display the game
+   	      ii. Display the hint
+   	      iii. Player goes again until player is wrong
+   	      iv. If game over, compare score and declare winner
+   	  3. If letter picked was a vowel, subtract 250 from player's money
+   	  4. If the the letter is incorrect,
+      	 i. update player score
+      	 ii. computer turn
+      5. letter is removed from list of available characters
+    b. Player guesses phrase
+       a. Add $1000 if computer has no money
+          or else add the difference between computer's money
+          and player's money + 100
+6. Computer Turn
+7. Computer spins and the value gets stored to a pending_money variable
+	i.  If bankrupt? then computer loses money
+	ii. If Lose_a_turn? then it becomes player's turn
+8. Computer Can only pick constanants if computer has less than 250
+   a. If there is more than 3 "_ ", then computer picks random letter or else
+   	  Computer takes a guess at winning phrase
+   	  i. When computer guesses the right phase
+   	  	  Add $1000 if computer has no money
+          or else add the difference between player's money
+          and computer's money + 100
+   b. If the letter picked is correct
+   	  i. Display game
+   	  ii. Update computer_money
+   	  iii. Computer goes again until he gets one wrong
+   	  iv. If game over, compare score and declare winner
+   	  v. When computer has $250 or more he can pick a vowel
+   	  vi.  If three or more vowels exist already, then pick a random letter
+   	  vii. If game over, compare score and declare winner
+   c. If incorrect letter
+   	  i. Update score
+   	  ii. Player turn
+9. Ask to play again
+
+
+Add input validation if player picks a letter chosen already
+
+Bonus features
+
+1. When we pick a letter and it happens to be in the index of 0, it needs to be capitalized
+
+  If letter is at index 0, capitalize
+
---
+++ wheel.rb
@@ -0,0 +1,178 @@
+require 'pry'
+
+system 'clear'
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+VOWELS = ["a", "e", "i", "o", "u"].freeze
+
+spinner_values =
+  ["Bankrupt", "Lose a turn",
+   300, 300, 300,
+   300, 300, 300,
+   350, 400, 400,
+   450, 500, 500,
+   550, 600, 600,
+   600, 700, 800,
+   800, 900, 900]
+
+RIDDLES = { "dances with wolves" => "Movie",
+            "kill two birds with one stone" => "Idiom",
+            "ain't nuthin but a g thang" => "Song" }.freeze
+
+def no_vowel_allowed(char, money)
+  VOWELS.include?(char) && money < 250
+end
+
+def initialize_game(game, winning_phrase)
+  winning_phrase = winning_phrase.split(//)
+  winning_phrase.each_with_index do |char, index|
+    if /[[:alpha:]]/ =~ char
+      game[index] = "_ "
+    else
+      game[index] = char
+    end
+  end
+  game.values.join("")
+end
+
+def spin(spinner_values)
+  spinner_values.sample
+end
+
+def display_hint(hint)
+  puts
+  puts "It's a #{hint}"
+end
+
+def not_a_letter(letter)
+  letter.to_i > 0 || letter.to_i == "0"
+end
+
+def unavailable_letter(chars, letter)
+  !chars.include?(letter)
+end
+
+def no_letter?(winning_phrase, letter)
+  !winning_phrase.include?(letter)
+end
+
+def update_game(game, winning_phrase, char)
+  winning_phrase = winning_phrase.split(//)
+  winning_phrase.each_with_index do |character, index|
+    if character == char.upcase
+      game[index] = char.upcase + " "
+    elsif character == char.downcase
+      game[index] = char + " "
+    elsif !(/[[:alpha:]]/ =~ character)
+      game[index] = character
+    end
+  end
+  game.values.join("")
+end
+
+def letter_count(winning_phrase, letter)
+  letter.downcase!
+  count = winning_phrase.count(letter)
+  if count < 1
+    false
+  elsif count == 1
+    "There is #{count} '#{letter}' in this riddle"
+  else
+    "There are #{count} #{letter}'s in this riddle"
+  end
+end
+
+def bankrupt?(pending_money)
+  pending_money == "Bankrupt"
+end
+
+def lose_a_turn?(pending_money)
+  pending_money == "Lose a turn"
+end
+
+def add_money_for_guess(player, computer)
+  if computer == 0 || player > computer
+    player + 1000
+  else
+    player + (computer - player) + 100
+  end
+end
+
+def game_over?(game)
+  !game.value?("_ ")
+end
+
+loop do
+  chars = ["a", "b", "c",
+           "d", "e", "f",
+           "g", "h", "i",
+           "j", "k", "l",
+           "m", "n", "o",
+           "p", "q", "r",
+           "s", "t", "u",
+           "v", "w", "x",
+           "y", "z"]
+
+  game = Hash.new
+  player_score = 0
+  computer_score = 0
+  prompt "Welcome to Wheel of fortune"
+  winning_phrase = RIDDLES.keys.sample
+  letter_screen = initialize_game(game, winning_phrase)
+  hint = RIDDLES[winning_phrase]
+  loop do
+    p letter_screen
+    break if game_over?(game)
+    display_hint(hint)
+    pending_money = spin(spinner_values)
+    puts "spinning."
+    puts "spinning.."
+    puts "spinning..."
+    puts "You have $#{player_score}"
+    if bankrupt?(pending_money)
+      puts "Ouch! Looks like you bankrupted"
+      player_score = 0
+      break
+    elsif lose_a_turn?(pending_money)
+      puts "Looks like you lost your turn"
+      break
+    else
+      puts "It's $#{pending_money}, type 'L' to pick a letter or 'G' to guess the winning phrase"
+    end
+    decision = gets.chomp.upcase
+    if decision == "L"
+      loop do
+        puts "Pick a Letter"
+        char = gets.chomp
+        if no_vowel_allowed(char, player_score)
+          puts "You need more than $250 to pick a vowel"
+        elsif not_a_letter(char)
+          puts "Please type a letter a-z"
+        elsif unavailable_letter(chars, char)
+          puts "#{char} is already there"
+        elsif !letter_count(winning_phrase, char)
+          puts "There is no #{char} in this riddle"
+          break
+        else
+          letter_screen = update_game(game, winning_phrase, char)
+          player_score += pending_money
+          character_count = letter_count(winning_phrase, char)
+          puts character_count
+          chars.delete(char)
+          break
+        end
+      end
+    elsif decision == "G"
+      puts "Guess this riddle"
+      answer = gets.chomp
+      if answer == winning_phrase
+        puts "Congrats you won!"
+        player_score = add_money_for_guess(player_score, computer_score)
+        break
+      end
+    end
+  end
+end
